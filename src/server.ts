6a9cd138e4f4786f4edd67185ca9e8fca175d84b
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2023-05-15 21:14:12 -0300

    feat: create fastify basic setup

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.info("Helo world!");
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get("/", () => {
+  return "Hello World!";
+});
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("🚀 HTTP server running on http://localhost:3333");
+});
