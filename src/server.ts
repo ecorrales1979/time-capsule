c3ae479f3202caefe60f5af76d6b956e9957a5e5
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2023-05-15 22:17:28 -0300

    chore: add eslint and prettier

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
-const app = fastify();
+const app = fastify()
 
-app.get("/", () => {
-  return "Hello World!";
-});
+app.get('/', () => {
+  return 'Hello World!'
+})
 
-app.listen({ port: 3333 }).then(() => {
-  console.log("🚀 HTTP server running on http://localhost:3333");
-});
+app
+  .listen({ port: 3333 })
+  .then(() => {
+    console.log('🚀 HTTP server running on http://localhost:3333')
+  })
+  .catch((error: Error) => {
+    console.error(error)
+  })
